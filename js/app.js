102ca4f145fcc7914e7d1ab97826491459351433
jenniferjara <lisbet.jara@gmail.com>
2016-10-11 15:25:37 -0500

    cambios en el archivo js usando jQuery

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var iniciar = function(){
+// JavaScript
+
+/*var iniciar = function(){
 	var link = document.querySelectorAll(".span3 ul li a");
 	for(var i = 0; i < link.length; i++){
 		var url = link[i].getAttribute("href");
@@ -7,5 +9,21 @@ var iniciar = function(){
 		imagen.setAttribute("src", thumb);
 		link[i].appendChild(imagen)
 	}
+};
+window.addEventListener("load", iniciar);*/
+
+// jQuery
+
+var iniciar = function(){
+	var $enlaces = $("a");
+	$enlaces.each(interar);
 }
-window.addEventListener("load", iniciar);
+var interar = function(index, element){
+	var $element = $(element);
+	var linkVideo = $element.attr("href");
+	var linkImagen = youtube.generateThumbnailUrl(linkVideo);
+	var $imagen = $("<img>");
+	$imagen.attr("src", linkImagen);
+	$element.append($imagen);
+}
+$(document).ready(iniciar);
---
+++ readme.md
@@ -0,0 +1,19 @@
+#Introducción a JQuery
+
+En este ejercicio primero se realizo primero usando JavaScript y luego jQuery.
+
+Estas son las indicaciones del ejercicio:
+
+###Video Player
+
+1. Crear en array con todos los links de la página usando querySelectorAll(cssSlector).
+
+2. Iterar en este array y en cada iteración del loop debe:
+
+	* Encontrar el atributo 'href' usando getAttribute. y guardarlo en una variable.
+
+	* Generar una miniatura del URL usando youtube.generateThumbnailUrl(videoUrl).
+
+	* Crear una elemento img y cambiar su atributo 'src'.
+
+	*Agregar la imagen al link usando appendChild.
